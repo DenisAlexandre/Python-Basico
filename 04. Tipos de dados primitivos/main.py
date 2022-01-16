f0e3eedaea76e2c304d4b1a6ca1717509bd75ae1
DenisAlexandre <82685513+DenisAlexandre@users.noreply.github.com>
2022-01-16 00:46:52 -0300

    04. Tipos de dados primitivos

---
+++ 04. Tipos de dados primitivos/main.py
@@ -0,0 +1,20 @@
+"""
+Tipos de dados
+str - string
+int - inteiro
+float - real/ponto fluante
+bool  - booleano/lógico
+
+"""
+print('Denis', type('Denis'))
+print(10, type(10))
+print(25.23, type(25.23))
+print(10 == 10, type(10 == 10))
+
+print('Denis', type('Denis'), bool('Denis')) 
+print('Denis', float("10.50"))
+print(10 + 10 )
+print('10' + '10')
+
+print('Denis', type('Denis'))
+print(40, type(40))
