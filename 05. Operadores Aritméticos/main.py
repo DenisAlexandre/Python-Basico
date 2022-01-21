80ea5cab401e0b42461aae43ad8478efc23296db
denis <denis.usp07@gmail.com>
2022-01-20 23:19:05 -0300

    05. Operadores Aritméticos

---
+++ 05. Operadores Aritméticos/main.py
@@ -0,0 +1,11 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+print('Multiplicação * : ', 10 * 10)
+print('Adição + : ', 10 - 10)
+print('Subtração - : ', 10 - 5)
+print('Divisao / :', 10 / 2)
+
+print('Divisao inteira / :', 10 // 3)
+print('Potênciação / :', 2 ** 3)
+print('Resto da divisao / :', 2 % 3)
