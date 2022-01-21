dd64f163cefa8cd8014c637af10b3b9e976cc5d1
DenisAlexandre <82685513+DenisAlexandre@users.noreply.github.com>
2022-01-21 00:10:04 -0300

    06. Variáveis

---
+++ 06. Variáveis/main.py
@@ -0,0 +1,16 @@
+nome = "Denis"
+idade = 32
+altura = 1.75
+e_maior = idade > 18
+data_1 = True
+data_atual = 2019
+peso = 140
+imc = (peso/ (altura ** 2))
+
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('Altura: ', altura)
+print('É maior: ', e_maior)
+
+print(nome, 'tem', idade, 'de idade e seu imc é', imc)
+
