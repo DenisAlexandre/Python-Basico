fe9de73a45663febf2afde3115f7193576defbcd
denis <denis.usp07@gmail.com>
2022-01-24 23:50:53 -0300

    10. ondições IF, ELIF, e Else

---
+++ 10. IF, ELIF e ELSE + Booleans/main.py
@@ -0,0 +1,11 @@
+"""
+Condições IF, ELIF, e Else
+"""
+if False:
+    print('Verdadeiro')
+elif False:
+    print("Agora é verdadeiro")
+elif True:
+    print("Mais uma verdadeira")
+else:
+    print('Não é Verdadeiro')
