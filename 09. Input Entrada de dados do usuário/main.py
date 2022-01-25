aeefc7a7b260a8a6957c9681aafa27854f17c03a
DenisAlexandre <82685513+DenisAlexandre@users.noreply.github.com>
2022-01-24 23:03:54 -0300

    09. Input

---
+++ 09. Input Entrada de dados do usuário/main.py
@@ -0,0 +1,8 @@
+nome = input("Qual o seu nome?  ")
+idade = input("Qual o sua idade?  ")
+ano_nascimento = 2022 - int(idade)
+
+print(f'O usuário digitou {nome} e o tipo da variável é {type(nome)}')
+print(f'{nome} tem {idade} anos.')
+
+
