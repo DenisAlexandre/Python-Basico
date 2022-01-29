6ccd0424d2dfab365fa099a4bed34f1706818f14
DenisAlexandre <82685513+DenisAlexandre@users.noreply.github.com>
2022-01-29 11:19:36 -0300

    12. Operadores lógicos IF ELIF ELSE

---
+++ 12. Operadores lógicos IF ELIF ELSE/main.py
@@ -0,0 +1,19 @@
+"""Operadores Lógicos - Aula 
+and, or, not
+in e not in
+"""
+a = 2 
+b = 3 
+c = ''
+string = "teste"
+
+if b > a:
+    print('B é maior do que A')
+else:
+    print('A é maior do que B')
+    
+if not c:
+    print("Por favor, preencha o valor de B.")
+    
+if 'e' in string:
+    print("contem a letra e")
