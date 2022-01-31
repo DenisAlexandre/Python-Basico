108fbf90734c63014b8632f2bf592409cf39e0b6
DenisAlexandre <82685513+DenisAlexandre@users.noreply.github.com>
2022-01-30 23:30:29 -0300

    18. Indices e fatiamento de strings em Python

---
+++ 18. Indices e fatiamento de strings em Python/main.py
@@ -0,0 +1,11 @@
+#positivos  =  [012345678]
+texto       =  'Python s2'
+#negativos  = -[987654321]
+print(texto[2])
+print(texto[-9])
+nova_string = texto[2:6]
+print(nova_string)
+nova_string = texto[-2:]
+print(nova_string)
+nova_string = texto[::2]
+print(nova_string)
