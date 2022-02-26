f43cf3cd958b405163905443929b1c8d3ed52013
DenisAlexandre <82685513+DenisAlexandre@users.noreply.github.com>
2022-02-26 12:13:58 -0300

    30. Expressão condicional com operador OR

---
+++ 30. Expressão condicional com operador OR/main.py
@@ -0,0 +1,16 @@
+nome = input('Qual o seu nome? ')
+print(nome or 'Você nao digitou nada')
+
+a = 0
+b = None 
+c = False
+d = [] 
+e = {}
+f = 22
+g = 'Denis'
+
+variavel = a or b or c or d or e or f or g 
+
+print(variavel)
+
+    
